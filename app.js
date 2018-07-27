7890f796722ea4616cc196fdfe842e7e32c81c19
warrioryoko <miangpellegri@gmail.com>
2018-07-26 17:10:11 -0700

    Testing user input values

--- app.js
+++ app.js
@@ -7,27 +7,30 @@ var chosenWord = getWord(copy);
 function getWordIndex (max){
     return Math.floor(Math.random() * max); 
         
-    }
-console.log(getRandomInt(0, words.length));
+}
 
 function getWord(){
     var index = getWordIndex(copy.length);
     var word = copy[index];
     //return word.splice();
-    console.log(word);
+    //console.log(word);
 }
 
 
     //console.log(copy);
-    console.log(chosenWord);
+    //console.log(chosenWord);
 
 function createWordArray(){
 
 }
 
 function guessLetter(){
-    var letter = document.getElementById("guess-letter")
+    var letter = document.getElementById("letter-guessed");
+    console.log(letter.value);
+    var sanitizedInput = letter.value.charCodeAt(0);
+    console.log(sanitizedInput);
 
+    return false;
 }
   //  function getRandomWord() {
     
