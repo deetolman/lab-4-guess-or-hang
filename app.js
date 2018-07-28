1c3e7032b41d02fc372dce28bc97b77659758ce6
warrioryoko <miangpellegri@gmail.com>
2018-07-28 13:55:14 -0700

    Implemented testing for win and lose conditions, and corresponding messages on the page.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 /* globals words */
 /* exported getWordIndex */
 /* exported getWord */
-/* exported testing */
 /* exported displaySpaces */
 
+var correctCounter = 0;
 var copy = words.slice();
 //var chosenWord = displaySpaces(copy);
 var chosenWord = '';
@@ -30,8 +30,8 @@ function displaySpaces(){
     console.log(html);
    
     var displayWord = document.getElementById ('hidden-letters');
-   displayWord.innerHTML = html;
-   return false;
+    displayWord.innerHTML = html;
+    return false;
 }
 
 function guessLetter(){
@@ -40,30 +40,26 @@ function guessLetter(){
     var userMessage = document.getElementById("message");
     var displayWord = document.getElementById('hidden-letters');
     correctGuesses = false;
-    var correctCounter = 0;
+    
 
     for(var j = 0; j < letters.length; j++) {
         var displayCharacters = document.getElementById(j);
         if(userInput === letters[j]){
             correctCounter++;
-            displayCharacters.innerHTML = letters[j];
-            if(correctCounter === letters.length){
-                userMessage.innerText = 'Congratulations! You\'ve escaped the hangman\'s noose!';
-                console.log(correctCounter);//win condition
-            }
-            else{
-                userMessage.innerText = "Good job! That letter was a match!";
-            }
             correctGuesses = true;
+            displayCharacters.innerHTML = letters[j];
+            userMessage.innerText = "Good job! That letter was a match!";
             console.log(correctCounter);
         }
-        console.log(correctGuesses);
-        // else if(userInput !== letters[j]) {
-        //     guess = false;
-        // }
+
     }
+    if(correctCounter === letters.length){
+        userMessage.innerText = 'Congratulations! You\'ve escaped the hangman\'s noose!';
+        console.log(correctCounter);//win condition
+    }
+
     if(correctGuesses === false){
-        console.log('hit this code');
+        console.log('User made an incorrect guess!');
         userMessage.innerText = "Sorry, not a match!";
         var wrongLetter = document.getElementById("wrong-list");
         wrongLetter.innerText += ' ' + userInput;
@@ -71,6 +67,10 @@ function guessLetter(){
         console.log(guessesLeft);
             //if statement for losing endgame situation
             //replace userMessage.innerText with "you lose blah blah"
+    }
+
+    if(guessesLeft === 0) {
+        userMessage.innerText = "You ran out of guesses... You lose!"
     }
 
 
