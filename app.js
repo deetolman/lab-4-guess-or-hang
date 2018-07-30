c19519d2bbb234b538f1ebe448ea3b1d9f410f87
warrioryoko <miangpellegri@gmail.com>
2018-07-30 02:27:39 -0700

    Finished all functionality. Will refactor to clean up code, and add styles.

--- app.js
+++ app.js
@@ -1,33 +1,52 @@
 /* globals words */
-/* exported getWordIndex */
-/* exported getWord */
-/* exported displaySpaces */
+/* globals variables */
+/* exported loadWord */
+/* exported guess */
+/* exported reset */
 
 var correctCounter = 0;
-var copy = words.slice();
-//var chosenWord = displaySpaces(copy);
-var chosenWord = '';
-function getWordIndex (max){
-    return Math.floor(Math.random() * max); 
-        
-}
+var word;
+var letters;
 
 var guessButton = document.getElementById('guess-button');
 var startButton = document.getElementById('start-button');
 var guessesNum  = document.getElementById('guesses-left');
-var index = getWordIndex(copy.length);
-var word = copy[index];
-var letters = word.split('');
+var guessesMade = document.getElementById('guesses-made');
+var badGuesses = document.getElementById('wrong-list');
+var gameWins = document.getElementById('wins');
+var gameLosses = document.getElementById('losses');
+var getMeOutOfHere = document.getElementById('quit-button');
+var gallows1 = document.getElementById('gallows1');
+var gallows2 = document.getElementById('gallows2');
+var gallows3 = document.getElementById('gallows3');
+var gallows4 = document.getElementById('gallows4');
+var gallows5 = document.getElementById('gallows5');
+var gallows6 = document.getElementById('gallows6');
+var gallows7 = document.getElementById('gallows7');
+
 var guessesLeft = 6;
 var correctGuesses = 0;
-var wrongLetters = [];
+var totalGuesses;
+var totalWins = 0;
+var totalLosses = 0;
+var inputTried;
+var lettersTried = [];
+var hangedMan = [0,1,2,3,4,5];
 
-function displaySpaces(){
-    // var index = getWordIndex(copy.length);
-    // var word = copy[index];
-    // var letters = word.split('');
+function loadWord(){
+    
+    function getWordIndex (max){
+        return Math.floor(Math.random() * max); 
+            
+    }
+    
+    var copy = words.slice();
+    var index = getWordIndex(copy.length);
+    word = copy[index];
+    letters = word.split('');
+    var html = '';
     console.log(letters);
-    var html = ''; 
+    
     for(var i = 0; i < letters.length; i++){
             html += `<label id = ${i}> _ </label>`;
         }
@@ -35,14 +54,21 @@ function displaySpaces(){
    
     var displayWord = document.getElementById ('hidden-letters');
     displayWord.innerHTML = html;
-    //startButton.disabled = true;
+    gallows1.innerText = "  +---+  ";
+    gallows2.innerText = "  |   |  ";
+    gallows3.innerText = "      |  ";
+    gallows4.innerText = "      |  ";
+    gallows5.innerText = "      |  ";
+    gallows6.innerText = "      |  ";
+    gallows7.innerText = "=========";
+    startButton.disabled = true;
 
     return false;
 }
 
-function guessLetter(){
+function guess(){
     var userInput = document.getElementById('letter-guessed').value;
-    console.log(userInput);
+    console.log("The user entered: " + userInput);
     var userMessage = document.getElementById('message');
     var displayWord = document.getElementById('hidden-letters');
     correctGuesses = false;
@@ -64,8 +90,8 @@ function guessLetter(){
         return false;
     }
 
-    for(var k = 0; k < wrongLetters.length; k++) {
-        if(userInput === wrongLetters[k]) {
+    for(var k = 0; k < lettersTried.length; k++) {
+        if(userInput === lettersTried[k]) {
             console.log("ding dong");
             userMessage.innerText = 'Hey! You already tried that! Pick another letter...';
             return false;
@@ -80,38 +106,76 @@ function guessLetter(){
             correctGuesses = true;
             displayCharacters.innerHTML = letters[j];
             userMessage.innerText = 'Good job! That letter was a match!';
-            wrongLetters.push(userInput);
-            console.log(correctCounter);
+            lettersTried.push(userInput);
+            console.log("Number of letters in WORD correctly guessed: " + correctCounter);
         }
 
     }
 
     if(correctCounter === letters.length){
-        userMessage.innerText = 'Congratulations! You\'ve escaped the hangman\'s noose!';
+        userMessage.innerText = 'Congratulations! You\'ve escaped the hangman\'s noose! Click the "Reset Game" button to play again.';
         guessButton.disabled = true;
+        totalWins++;
+        gameWins.innerText = `Wins: ${totalWins}`;
         console.log(correctCounter);//win condition
     }
 
     if(correctGuesses === false){
         console.log('User made an incorrect guess!');
         userMessage.innerText = 'Sorry, not a match!';
-        var badGuesses = document.getElementById('wrong-list');
         badGuesses.innerText += ' ' + userInput;
         guessesLeft--;
-        wrongLetters.push(userInput);
-        console.log(wrongLetters);
+        lettersTried.push(userInput);
         console.log(guessesLeft);
-            //if statement for losing endgame situation
-            //replace userMessage.innerText with "you lose blah blah"
+
+        switch (guessesLeft) {
+            case 5:
+                gallows1.innerText = "  +---+  ";
+                gallows2.innerText = "  |   |  ";
+                gallows3.innerText = "  o   |  ";
+                gallows4.innerText = "      |  ";
+                gallows5.innerText = "      |  ";
+                gallows6.innerText = "      |  ";
+                gallows7.innerText = "=========";
+                break;
+            case 4:
+                gallows4.innerText = "  |   |  ";
+                break;
+            case 3:
+                gallows4.innerText = " /|   |  ";
+                break;
+            case 2:
+                gallows4.innerText = " /|\\  |  ";
+                break;
+            case 1:
+                gallows5.innerText = " /    |  ";
+                break;
+            case 0:
+                gallows5.innerText = " / \\  |  ";
+                break;
+        }
+
     }
 
+    inputTried = new Set(lettersTried);
+    console.log("This is the array of all letters tried so far... " + [...inputTried]);
+    totalGuesses = inputTried.size;
+    guessesMade.innerText = totalGuesses;
     guessesNum.innerText = guessesLeft;
 
     if(guessesLeft === 0) {
-        userMessage.innerText = 'You ran out of guesses... You lose!';
+        userMessage.innerText = 'You ran out of guesses... You are DEAD! You lose! Click the "Reset Game" button to play again.';
         guessButton.disabled = true;
+        totalLosses++;
+        gameLosses.innerText = `Losses: ${totalLosses}`;
     }
 
+    return false;
+}
+
+function reset() {
+    loadWord();
+    guessButton.disabled = false;
 
     return false;
 }
